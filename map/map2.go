ccd20913e2173049934a2ebf2b2b2b284eef9378
xiaok1051 <xiaok1051@163.com>
2018-04-18 23:50:22 +0800

    map2

--- map/map2.go
+++ map/map2.go
@@ -4,3 +4,24 @@ package main
 import (
 	"fmt"
 )
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex
+
+func main() {
+	m = make(map[string]Vertex)
+	m["Bell Labs"] = Vertex{40.68433, -74.39967}
+	m["Bei Jing"] = Vertex{39.26, -115.25}
+
+	fmt.Println(m)
+	fmt.Println("----------I'm just a line.----------")
+
+	for lat, _ := range m {
+		fmt.Println(lat, ": ", m[lat].Lat, m[lat].Long)
+	}
+	delete(m, "Bei Jing")
+	fmt.Println(m)
+}
