52041da97124384e99babb07c3cc4d14567c9d7e
xiaok1051 <xiaok1051@163.com>
2018-05-03 22:57:36 +0800

    range2

---
+++ range/range2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	m := map[int]string{1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
+	s := make([]int, len(m))
+	i := 0
+	for k, _ := range m {
+		s[i] = k
+		i++
+	}
+	sort.Ints(s)
+	fmt.Println(s)
+}
