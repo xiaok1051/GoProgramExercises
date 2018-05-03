82e4a3ec8f1d844f94e56aafa02b2c6f5c9db3b4
xiaok1051 <xiaok1051@163.com>
2018-05-03 23:56:47 +0800

    Panic and Recover

---
+++ PanicRecover/PanciRecover.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	A()
+	B()
+	C()
+}
+
+func A() {
+	fmt.Println("Func A")
+}
+
+func B() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("Recover in B")
+		}
+	}()
+	panic("Panic in B")
+}
+
+func C() {
+	fmt.Println("Func C")
+}
