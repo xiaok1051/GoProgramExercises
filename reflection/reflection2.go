18e76922efef76cc60a63c429273534c9d8a9404
xiaok1051 <xiaok1051@163.com>
2018-05-08 00:05:41 +0800

    reflection2

---
+++ reflection/reflection2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+type Manager struct {
+	User
+	title string
+}
+
+func main() {
+	m := Manager{User: User{1, "OK", 12}, title: "123"}
+	t := reflect.TypeOf(m)
+
+	fmt.Printf("%#v\n", t.FieldByIndex([]int{0, 0}))
+}
