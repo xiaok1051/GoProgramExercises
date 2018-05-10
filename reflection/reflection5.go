00b2a8a1fc128c1ff9083504c4db55b7ebc4b9a3
xiaok1051 <xiaok1051@163.com>
2018-05-11 00:16:25 +0800

    reflection5

---
+++ reflection/reflection5.go
@@ -0,0 +1,26 @@
+/**/
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+func (u User) Hello(name string) {
+	fmt.Println("Hello", name, ", my name is", u.Name)
+}
+
+func main() {
+	u := User{1, "OK", 12}
+	v := reflect.ValueOf(u)
+	mv := v.MethodByName("Hello")
+
+	args := []reflect.Value{reflect.ValueOf("joe")}
+	mv.Call(args)
+}
