0e8617139f6d7182e351a8024c3c773666e8fb49
xiaok1051 <xiaok1051@163.com>
2018-05-24 00:12:07 +0800

    goHttp3

---
+++ goWebDemo/temp3.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+var mux map[string]func(rw http.ResponseWriter, req *http.Request)
+
+func main() {
+	server := http.Server{
+		Addr:        ":8080",
+		Handler:     &myHandler{},
+		ReadTimeout: 5 * time.Second,
+	}
+
+	mux = make(map[string]func(rw http.ResponseWriter, req *http.Request))
+	mux["/hello"] = sayHello
+	mux["/bye"] = sayBye
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
+
+	io.WriteString(w, "URL: "+r.URL.String())
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello world, this is version 3.")
+}
+
+func sayBye(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Bye Bye, this is version 3.")
+}
